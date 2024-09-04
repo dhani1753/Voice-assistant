bd2682403414f209c73c4aa1fe526daa8932a011
dhani1753 <133853460+dhani1753@users.noreply.github.com>
2024-09-04 21:07:07 +0530

    Add files via upload

---
+++ package dhanashri.java
@@ -0,0 +1,11 @@
+package dhanashri
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main (string[] args){
+        Scanner sc= new scanner(system.in);
+        int a = sc.nextLine()
+        System.out.println(a);
+    }
+}
